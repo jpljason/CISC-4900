92913a683f7c95836d98121ba11c2108577c4770
SH13579 <stanleyh3131@gmail.com>
2024-09-22 22:12:45 -0400

    leaflet map

--- Website/front-end/src/App.js
+++ Website/front-end/src/App.js
@@ -5,6 +5,7 @@ import Overview from "./components/Overview";
 import Evaluation from "./components/Evaluation";
 import AboutUs from "./components/About-Us";
 import HomePage from "./components/HomePage";
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
--- Website/front-end/src/components/Evaluation.js
+++ Website/front-end/src/components/Evaluation.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "../styles/evaluation.css"
+import { MapContainer, TileLayer } from "react-leaflet"
 
 export default function Evaluation() {
 
   return (
-    <section id="evaluation">
+    <section>
       <h1>EVALUATION</h1>
-    </section>
+      <center>
+        <MapContainer center={[40.631015606268996, -73.95131852800579]} zoom={20} className="map-container">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+      </center>
+      <h1>EVALUATION</h1>
+      <h1>EVALUATION</h1>
+      <h1>EVALUATION</h1>
+      <h1>EVALUATION</h1>
+      <h1>EVALUATION</h1>
+      <h1>EVALUATION</h1>
+      <h1>EVALUATION</h1>
 
+    </section>
   )
   
 }
